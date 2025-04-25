256a183be81f801e10036104daad796c3fb0e334
dattu095 <dattu7334@gmail.com>
2025-04-25 04:45:57 +0000

    updated code

--- pico/sensors/sw420.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-
-
-class SW420:
-    def __init__(self, out_pin):
-        self.sensor = Pin(out_pin, Pin.IN)
-    
-    def get_value(self):
-        return self.sensor.value()
